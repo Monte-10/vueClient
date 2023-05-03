326ae7525c71181d458df8b642628dd658a014cc
Monte-10 <alejandro.monterrubio@estudiante.uam.es>
2023-05-03 13:08:33 +0200

    Esto deberia ir

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
 // import './assets/main.css'
 
-const app = createApp(App)
+const app = createApp(App);
 
-import "../node_modules/bootstrap/dist/js/bootstrap.js"
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../node_modules/bootstrap/dist/js/bootstrap.js";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-app.use(router)
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,28 @@
 //src/router/index.js
-import { createRouter, createWebHistory } from 'vue-router'
-import VistaUnirse from '../views/VistaUnirse.vue'
-import VistaEsperar from '../views/VistaEsperar.vue'
-import VistaJugar from '../views/VistaJugar.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import VistaUnirse from "../views/VistaUnirse.vue";
+import VistaEsperar from "../views/VistaEsperar.vue";
+import VistaJugar from "../views/VistaJugar.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'main',
-      component: VistaUnirse
+      path: "/",
+      name: "main",
+      component: VistaUnirse,
     },
     {
-      path: '/wait',
-      name: 'wait',
-      component: VistaEsperar
+      path: "/wait",
+      name: "wait",
+      component: VistaEsperar,
     },
     {
-      path: '/play',
-      name: 'play',
-      component: VistaJugar
-    }
-  ]
-})
+      path: "/play",
+      name: "play",
+      component: VistaJugar,
+    },
+  ],
+});
 
-export default router
+export default router;
